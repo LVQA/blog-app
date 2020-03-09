a36e9ede1ab8a9530a9c3805de051f242b6885dc
Lê Văn Quang An <ladvodoi@gmail.com>
2020-03-09 20:12:51 +0700

    update

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php 
 	session_start();
 
-	// connect to database
-       // coming soon...
+	$conn = mysqli_connect("localhost", "root", "", "blog-app");
+
+	if (!$conn) {
+		die("Error connecting to database: " . mysqli_connect_error());
+	}
 
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
 	define('BASE_URL', 'http://localhost/blog-app/');
